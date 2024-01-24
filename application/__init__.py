393e33b3b04d9a9a78fa5f3bbc65b34df60d75d9
thebackendie <134281031+thebackendie@users.noreply.github.com>
2024-01-23 23:44:27 -0800

    add hashing

--- application/__init__.py
+++ application/__init__.py
@@ -2,6 +2,8 @@ from http import HTTPStatus
 import sqlalchemy
 from flask import Flask, make_response, jsonify
 from flask_restx import Api
+from flask_bcrypt import Bcrypt
+from flask_migrate import Migrate
 from .config.v1.config import config_dict
 from .controllers.v1.auth import auth_namespace
 from .utils import db
@@ -12,6 +14,8 @@ def create_app(config=config_dict['dev']):
     app.config.from_object(config)
     api = Api(app)
     db.init_app(app)
+    migrate = Migrate(app, db)
+    bcrypt = Bcrypt(app)
 
     # Routes
     api.add_namespace(auth_namespace, path="/api/v1/auth")
